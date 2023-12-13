80f18f901cf55e96c44ae61af0d748282c6eab8d
Radeah <113103602+Radeah@users.noreply.github.com>
2023-12-13 20:41:19 +0000

    ill fix dis later

--- Assets/Scips/DragAndDrop.cs
+++ Assets/Scips/DragAndDrop.cs
@@ -21,6 +21,7 @@ public class DragAndDrop : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
             Vector2 touchPosition = Camera.main.ScreenToViewportPoint(touch.position);
+
             if (touch.phase == TouchPhase.Began)
             {
                 Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
